2bde1659e0575f84d9d41fe10de2fbddd819b49a
이승현 <leesh3288@naver.com>
2018-09-03 22:36:35 +0900

    TWCTF Writeups

---
+++ 2018/TWCTF/SimpleAuth.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'flag.php';
+
+if (!empty($_SERVER['QUERY_STRING'])) {
+    $query = $_SERVER['QUERY_STRING'];
+    $res = parse_str($query);
+    if (!empty($res['action'])){
+        $action = $res['action'];
+    }
+}
+
+if ($action === 'auth') {
+    if (!empty($res['user'])) {
+        $user = $res['user'];
+    }
+    if (!empty($res['pass'])) {
+        $pass = $res['pass'];
+    }
+
+    if (!empty($user) && !empty($pass)) {
+        $hashed_password = hash('md5', $user.$pass);
+    }
+    if (!empty($hashed_password) && $hashed_password === 'c019f6e5cd8aa0bbbcc6e994a54c757e') {
+        echo $flag;
+    }
+    else {
+        echo 'fail :(';
+    }
+}
+else {
+    highlight_file(__FILE__);
+}
